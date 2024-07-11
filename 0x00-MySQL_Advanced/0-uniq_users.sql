f627b7d41d081126138bb0020c70241ffee36beb
CHEGEBB <chegephil24@gmail.com>
2024-07-11 17:28:01 +0300

    feat: Add unique constraint to email column in users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
---script that creates a table users
---with attributes id,email,name
+-- SQL script that creates a table users
+
+-- Atributes id, email, name
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
