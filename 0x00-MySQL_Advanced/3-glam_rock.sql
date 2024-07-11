a58ccc846910c0a31734153cb6a1e722e4a321c4
CHEGEBB <chegephil24@gmail.com>
2024-07-11 18:21:22 +0300

    Refactor SQL script for selecting bands with Glam rock as their main style and ranking them by lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,11 @@
 -- Select bands with Glam rock as their main style
-SELECT band_name, IFNULL(2022 - formed, 0) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', style) > 0
-ORDER BY lifespan DESC;
+SELECT 
+    band_name, 
+    IFNULL(2022 - formed, 0) AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    FIND_IN_SET('Glam rock', style) > 0
+ORDER BY 
+    lifespan DESC;
+    
