03c13916044068fae028164bf489a039e1058cb2
CHEGEBB <chegephil24@gmail.com>
2024-07-11 17:49:38 +0300

    ```text Refactor SQL script for ranking bands by number of fans
    The code changes in the "2-fans.sql" file refactor the SQL script for ranking bands by the number of fans. The table name has been changed from "bands" to "metal_bands" to reflect the actual table name in the database. This change ensures that the script accurately calculates the number of fans for each band based on their origin and orders the results in descending order.

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- SQL script that ranks country origins
 -- of bands by the number of fans, based on the provided table dump
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
