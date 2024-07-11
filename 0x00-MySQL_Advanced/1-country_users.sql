9465cea7ac5ee78be3ff51ae4d26303d142a1b6a
CHEGEBB <chegephil24@gmail.com>
2024-07-11 17:43:38 +0300

    feat: Add unique constraint to email column in users table
    The code changes modify the SQL script for creating the "users" table. The "email" column is now defined as unique and not null. This change ensures that each user's email is unique in the database, which is important for user management functionality.

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
--- This sql script creates a table users with 
---columns id, email, and name and country
+-- This sql script creates a table users
+-- columns id, email, and name and country
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
