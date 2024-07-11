57632f6ef36c8386db19d12b64d49526d5a806ce
CHEGEBB <chegephil24@gmail.com>
2024-07-11 17:24:30 +0300

    Create users table with unique email constraint☑️

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 --script that creates a table users
 --with attributes id,email,name
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255)UNIQUE NOT NULL,
     name VARCHAR(255)
