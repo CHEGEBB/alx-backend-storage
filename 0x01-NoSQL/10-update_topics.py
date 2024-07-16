2f5b24b58d978a2375f5f93bdb510330fd404d2b
CHEGEBB <chegephil24@gmail.com>
2024-07-16 21:15:00 +0300

    nosql

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-""" 10-update_topics.py """
+"""This file contains the function for task 10.
+"""
 
 
 def update_topics(mongo_collection, name, topics):
-    '''
-    Changes all topics of a school document based on the name.
-    '''
+    """This function updates all topics of a school document based on the name
+    """
     mongo_collection.update_many(
         {'name': name},
         {'$set': {'topics': topics}}
