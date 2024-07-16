0e2ba6a729f01e193a09b7fa0e6137aac71d4351
CHEGEBB <chegephil24@gmail.com>
2024-07-16 21:14:04 +0300

    NEW

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,7 +5,6 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-    """
-    Inserts a new document in a collection based on kwargs
+    """This function inserts a new document in a collection based on kwargs
     """
     return mongo_collection.insert_one(kwargs).inserted_id
