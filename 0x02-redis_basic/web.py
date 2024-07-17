28e030148b4973ee26ac525afe1524d322d28e0f
CHEGEBB <chegephil24@gmail.com>
2024-07-17 18:17:21 +0300

    chore: Refactor web.py for better readability and maintainability

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,36 @@
 #!/usr/bin/env python3
-'''A module with tools for caching and tracking web requests.
-'''
-import redis
+"""
+web cache and tracker
+"""
 import requests
+import redis
 from functools import wraps
-from typing import Callable
+
+store = redis.Redis()
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance for caching and tracking.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Decorator to cache the output of data-fetching functions.
-    '''
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
-    def invoker(url: str) -> str:
-        '''Caches the output of the method and tracks the request count.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
 
 
-@data_cacher
+@count_url_access
 def get_page(url: str) -> str:
-    '''Fetches the content of a URL, caches the response,
-      and tracks the request.
-    '''
-    return requests.get(url).text
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
